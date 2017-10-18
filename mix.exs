708b47c1c6f46ea5ad1f4d3a2eb51d0ed05d39fc
Tran Xuan Quang <txquang22@gmail.com>
2017-10-18 15:46:39 +0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule BambooPostmark.Mixfile do
   end
 
   defp deps do
-    [{:bamboo, "~> 0.5"},
+    [{:bamboo, "~> 0.8"},
      {:hackney, "~> 1.6"},
      {:poison, ">= 1.5.0"},
      {:plug, "~> 1.0"},
